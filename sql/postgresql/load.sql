b40aba3e901daf1a96af1bb8a458d8dff2764066
Neophytos Demetriou <k2pts@phigita.net>
2001-09-15 21:57:00 +0000

    latest pg and openfts-tcl

--- sql/postgresql/load.sql
+++ sql/postgresql/load.sql
@@ -1,2 +1,2 @@
-\i /usr/local/src/postgresql-7.1.2/contrib/intarray/_int.sql
-\i /usr/local/src/Search-OpenFTS-tcl-0.1/func_pgsql/create_func.sql
+\i /usr/local/src/postgresql-7.1.3/contrib/intarray/_int.sql
+\i /usr/local/src/Search-OpenFTS-tcl-0.2/func_pgsql/create_func.sql
